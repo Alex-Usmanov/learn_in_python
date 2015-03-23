b30c273ab8825ea75ab9f1ac4788b058e85fbb7b
dodoru <nreazy013@gmail.com>
2015-03-23 21:30:46 +0800

    Update drawCanvas.py

--- drawCanvas.py
+++ drawCanvas.py
@@ -32,21 +32,11 @@ def drawLine(x1, y1, x2, y2):
         for i in range(x2-x1+1):
             drawPixel(x1+i,y1)
     else:
+        k=(y2-y1)/(x2-x1)
+        d=(y1*x2-y2*x1)/(x2-x1)
         for i in range(x2 - x1 + 1):
-            drawPixel(x1 + i, (y2 - y1) / (x2 - x1) * (x1 + i) + (y1 * x2 - y2 * x1) / (x2 - x1))
+            drawPixel(x1+i, k*(x1+i)+d)
 
-'''
-def drawLine(x1,y1,x2,y2):
-    if x1 is x2:
-        for i in range(y2 - y1 + 1):
-            drawPixel(x1, y1 + i)
-    elif y1 is y2:
-        for i in range(x2-x1+1):
-            drawPixel(x1+i,y1)
-    else:
-      for i in range((x2-x1)*100):
-        drawPixel(x1+i/100,(y2-y1)/(x2-x1)*(x1+i/100)+(y1*x2-y2*x1)/(x2-x1))
-'''
 
 def drawTriangle(x1,y1,x2,y2,x3,y3):
     drawLine(x1,y1,x2,y2)
@@ -63,6 +53,12 @@ def drawRectangle(x1, y1, x2, y2):
         drawLine(x2,y1,x2,y2)
 
 
+'''
+def drawLine(x1,y1,x2,y2):
+	for i in range((x2-x1)*100):
+		drawPixel(x1+i/100,(y2-y1)/(x2-x1)*(x1+i/100)+(y1*x2-y2*x1)/(x2-x1))
+'''
+
 def test_drawline():
     #drawPixel(1, 3)
     #drawPixel(3, 9)
@@ -78,11 +74,11 @@ def test_drawRecangle():
     drawRectangle(40,50,80,90)
 
 def test_drawTriangle():
-    drawTriangle(2,4,2,7,6,4)
+    drawTriangle(20,40,20,55,40,40)
     #drawTriangle(2,5,3,6,8,12)
 
-test_drawline()
-#test_drawTriangle()
+#test_drawline()
+test_drawTriangle()
 #testdrawRecangle()
 
 c.display()
