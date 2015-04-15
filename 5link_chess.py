bb08f77d1cb1e35b51faa835106c6adf9e68e70b
dodoru <nreazy013@gmail.com>
2015-04-15 23:29:20 +0800

    Ok ：D

--- 5link_chess.py
+++ 5link_chess.py
@@ -19,42 +19,6 @@ def game_place_at(board, position, color):
     else:
         return "wrong position."
 
-'''
-def _diagonal_line0(board, position):
-    # (6,4)(7,3)(8,2)(9,1)(10,0)
-    x0, y0 = position
-    z0 = x0 + y0
-    global diagonal
-    x = 0
-    y = 0
-    for hor in board:
-        for ver in hor:
-            z = x + y
-            if z is z0:
-                diagonal = ''.join(board[y][x])
-            x += 1
-        y += 1
-    return diagonal
-
-
-def _diagonal_line1(board, position):
-    # (0,0)(1,1)(2,2)(3,3)(4,4)
-    size = len(board)
-    x0, y0 = position
-    global diagonal
-    z0 = x0 - y0 + size
-    x = 0
-    y = 0
-    for hor in board:
-        for ver in hor:
-            z = x - y + size
-            if z is z0:
-                diagonal = ''.join(board[y][x])
-            x += 1
-        y += 1
-    return diagonal
-'''
-
 def game_check(board, position):
     x, y = position
     color = board[y][x]
@@ -63,32 +27,22 @@ def game_check(board, position):
 
     horizontal = ''.join(board[y])
     vertical = ''.join(board[i][x] for i in range(size))
-    # diagonal1 = _diagonal_line0(board, position)
-    # diagonal2 = _diagonal_line1(board, position)
-    mini=min((x+y),size)
-    diagonal1=''.join(board[i][x+y-i] for i in range(mini))
+
+    if x+y<size:
+        diagonal1=''.join(board[i][x+y-i] for i in range(x+y))
+        # (0,14)(1,13)(2,12)(3,11)(4,10)
+    else:
+        diagonal1=''.join(board[x+y-i][i] for i in range(x+y-size+1,size))
+        # (2,14)(3,13)(4,12)(5,11)(6,10)
+        # 如果用diagonal1=''.join(board[x+y-i][i] for i in range(min(x+y,size))) 会溢出
+
     if y<=x:
         diagonal2=''.join(board[i][x+i-y] for i in range(size+y-x))
         # (0,1)(1,2)(2,3)(3,4)(4,5)
     else:
         diagonal2=''.join(board[y-x+i][i] for i in range(size+x-y))
-        # (1,0)(2,1)(3,2)(4,3)(5,4)
+        # (1,0)(2,1)(3,2)(4,3)(5,4)11
 
-    '''
-    for hor in board:
-        for ver in hor:
-            if (x0 + y0)==(x + y):
-                diagonal1 = ''.join(board[y0][x0])
-            x0 += 1
-        y0 += 1
-
-    for hor in board:
-        for ver in hor:
-            if (x0 - y0) ==(x - y):
-                diagonal2 = ''.join(board[y0][x0])
-            x0 -= 1
-        y0 -= 1
-    '''
     lines = [horizontal, vertical, diagonal1, diagonal2]
     for i in lines:
         if win in i:
@@ -105,7 +59,6 @@ def game_display(board):
     print('  ' + ''.join(map(lambda i: str(i).rjust(3), range(size))))
     for i in range(size):
         print(str(i).rjust(2) + '  ' + '  '.join(board[i]))
-
 
 def game_input():
     command = raw_input()
@@ -129,7 +82,6 @@ def main():
         color = colors[player]
 
         game_display(board)
-        # print(board[4][4])
         game_show_instruction(color)
 
         position = game_input()
@@ -147,7 +99,6 @@ def main():
             break
 
         player = (player + 1) % 2
-
 
 if __name__ == '__main__':
     import sys
