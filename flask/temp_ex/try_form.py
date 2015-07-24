b375a947253326a6265246fbe7a025c81f461fe3
dodoru <nreazy013@gmail.com>
2015-07-24 17:33:29 +0800

    4.6 form_flash

--- flask/temp_ex/try_form.py
+++ flask/temp_ex/try_form.py
@@ -26,10 +26,12 @@ class NameForm(Form):
 
 @app.route('/',methods=['GET','POST'])
 def index():
-    name=None
     form=NameForm()
     if form.validate_on_submit():
-        session['name']=form.name.data
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+            session['name'] = form.name.data
         return redirect(url_for('index'))
         # form.name.data=''
         # 清空
