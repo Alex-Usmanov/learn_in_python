e80206d936aa43854b718e2f7c42579b8cbb4902
dodoru <nreazy013@gmail.com>
2015-07-25 23:47:57 +0800

    4 form (ugly boostrap ) and a little flash

--- flask/temp_ex/hello.py
+++ flask/temp_ex/hello.py
@@ -1,10 +1,11 @@
 # coding:utf-8
 
-from flask.ext.wtf import Form
+from try_form import NameForm
 from flask.ext.sqlalchemy import SQLAlchemy
 import os
 import flask
-from flask import Flask, render_template
+from flask import Flask, render_template, session, redirect, url_for
+from flask.ext.bootstrap import Bootstrap
 
 # 5.5
 # def config_SQLite():
@@ -12,6 +13,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = flask.Flask(__name__)
 print app.config
+boostrap = Bootstrap(app)
+app.config['SECRET_KEY'] = 'hard to guess string'
 '''
 <Config {'JSON_AS_ASCII': True, 'USE_X_SENDFILE': False, 'SESSION_COOKIE_PATH': None, 'SESSION_COOKIE_DOMAIN': None, 'SESSION_COOKIE_NAME': 'session', 'LOGGER_NAME': '__main__', 'DEBUG': False, 'SECRET_KEY': None, 'MAX_CONTENT_LENGTH': None, 'APPLICATION_ROOT': None, 'SERVER_NAME': None, 'PREFERRED_URL_SCHEME': 'http', 'JSONIFY_PRETTYPRINT_REGULAR': True, 'TESTING': False, 'PERMANENT_SESSION_LIFETIME': datetime.timedelta(31), 'PROPAGATE_EXCEPTIONS': None, 'TRAP_BAD_REQUEST_ERRORS': False, 'JSON_SORT_KEYS': True, 'SESSION_COOKIE_HTTPONLY': True, 'SEND_FILE_MAX_AGE_DEFAULT': 43200, 'PRESERVE_CONTEXT_ON_EXCEPTION': None, 'SESSION_COOKIE_SECURE': False, 'TRAP_HTTP_EXCEPTIONS': False}>
 
